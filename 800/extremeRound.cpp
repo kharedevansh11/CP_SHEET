d0b9bf6e13d322de9290aaa030d5cff9f51a3079
Devansh Khare <devanshkhare11072004@gmail.com>
2025-01-09 23:24:58 +0530

    800 level questions completed

---
+++ 800/extremeRound.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <set>
+#include <iostream>
+#include <queue>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define inp(arr, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll A;                   \
+        cin >> A;               \
+        arr.push_back(A);       \
+    }
+#define show(arr)         \
+    for (auto i : arr)    \
+        cout << i << " "; \
+    cout << "\n";
+ll min(ll a, int b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+ll max(int a, ll b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+void seive(ll n)
+{
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+   if(n<=9){
+    cout<<n<<endl;
+    return;
+   }
+   ll ans = 9;
+   if(n>=10 && n<=99){
+        ans = ans + n/10;
+   }
+   else if(n>=100 && n<=999){
+        ans = ans + 9 + n/100;
+   }
+   else if(n>=1000 && n<=9999){
+    ans = ans + 9 + 9+n/1000;
+   }
+   else if(n>=10000 && n<=99999){
+     ans = ans + 9 + 9+ 9 + n/10000;
+    
+   }
+   else if(n>=100000 && n<=999999){
+     ans = ans + 9 + 9+ 9+9 + n/100000;
+    
+   }
+    cout<<ans<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
