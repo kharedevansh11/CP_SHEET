5db5a9bafa1c61fdec276bee75e9bfaac5b20f1c
Devansh Khare <devanshkhare11072004@gmail.com>
2025-01-10 22:51:11 +0530

    started with 900 level questions

---
+++ 900/BalancedRound.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#include <set>
+#include <iostream>
+#include <queue>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define inp(arr, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll A;                   \
+        cin >> A;               \
+        arr.push_back(A);       \
+    }
+#define show(arr)         \
+    for (auto i : arr)    \
+        cout << i << " "; \
+    cout << "\n";
+ll min(ll a, int b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+ll max(int a, ll b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+void seive(ll n)
+{
+}
+
+void solve()
+{
+   ll n,k;
+   cin>>n>>k;
+   vll a;
+   inp(a,n);
+   ll rdiff=0;
+   ll ldiff=0;
+   sort(a.begin(),a.end());
+    ll maxTogether=0;
+    ll c=0;
+   for(int i=0;i<n-1;i++){
+    if(abs(a[i]-a[i+1]) > k){
+        c++;
+        maxTogether=max(maxTogether,c);
+        c=0;
+        rdiff = max(rdiff,(n-(i+1)));
+        ldiff = max(ldiff,i+1);
+        // cout<<i<<" ";
+    }
+    else{
+        c++;
+    }
+   }
+//    cout<<rdiff<<" "<<ldiff<<endl;
+   ll ans = min(rdiff,min(ldiff,n-maxTogether));
+   cout<<ans<<endl;
+   return;
+   
+
+   
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ 900/oddQueries.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+#include <set>
+#include <iostream>
+#include <queue>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define inp(arr, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll A;                   \
+        cin >> A;               \
+        arr.push_back(A);       \
+    }
+#define show(arr)         \
+    for (auto i : arr)    \
+        cout << i << " "; \
+    cout << "\n";
+ll min(ll a, int b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+ll max(int a, ll b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+void seive(ll n)
+{
+}
+
+void solve()
+{
+    ll n, q;
+    cin >> n >> q;
+    vll a;
+    inp(a, n);
+    ll s = 0;
+    for (auto it : a)
+    {
+        s += it;
+    }
+    vll pref(n + 1, 0);
+    pref[1] = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        pref[i + 1] = pref[i] + a[i];
+    }
+    //    show(a);
+    //    cout<<endl;
+    //    show(pref);
+
+    for (int i = 0; i < q; i++)
+    {
+        ll l, r, k;
+        cin >> l >> r >> k;
+        ll check = s - (pref[r] - pref[l-1]) + k*(r-l+1);
+        if(check%2!=0){
+            yes();
+        }
+        else{
+            no();
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
