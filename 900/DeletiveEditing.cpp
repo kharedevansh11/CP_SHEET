6086d8e0f6baaf4c661d9c1b3abd49ccc32819d2
Devansh Khare <devanshkhare11072004@gmail.com>
2025-01-12 02:36:32 +0530

    6 more questions of 900 rated

---
+++ 900/DeletiveEditing.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <set>
+#include <iostream>
+#include <queue>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define inp(arr, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll A;                   \
+        cin >> A;               \
+        arr.push_back(A);       \
+    }
+#define show(arr)         \
+    for (auto i : arr)    \
+        cout << i << " "; \
+    cout << "\n";
+ll min(ll a, int b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+ll max(int a, ll b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+void seive(ll n)
+{
+}
+
+void solve()
+{
+    // ll n;
+    // cin>>n;
+    string s,t;
+    cin>>s>>t;
+    ll j = t.size()-1;
+    vector<int>arr(26,0);
+    for(int i=s.size()-1;i>=0;i--){
+        if(s[i]==t[j] && arr[s[i]-'A']==0){
+            j--;
+        }
+        else{
+            arr[s[i]-'A']++;
+        }
+    }
+    if(j<0) yes();
+    else{
+        no();
+    }
+
+    
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ 900/NITdestroys.cpp
@@ -0,0 +1,2 @@
+// Main thing to check is , 
+//arr[] = {0,1,2,3} , then MEX(arr) = 4 => this is the smallest non negative number which is not present in arr.
